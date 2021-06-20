0e4e0dd1ab8ac243df28d0eafc438daa0e1d224b
Ruslan <ruslanehg@gmail.com>
2021-06-20 22:26:48 +0600

    try7

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,7 @@ export default defineConfig({
     ],
     //extensions: ['*', '.js', '.vue', '.json',]
   },
+  assetsDir: '/oxstablevite/',
+  publicDir: '/oxstablevite/',
+  base: '/oxstablevite/'
 })
